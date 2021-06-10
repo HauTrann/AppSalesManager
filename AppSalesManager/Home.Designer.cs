f0709774bc0c35e47019989911a2928bca491f2b
Hautv <Hauvantran97@gmail.com>
2021-06-10 23:17:22 +0700

    update

---
+++ AppSalesManager/Home.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AppSalesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Infragistics.Win.UltraWinToolbars.UltraToolbar ultraToolbar1 = new Infragistics.Win.UltraWinToolbars.UltraToolbar("Danh mục");
+            Infragistics.Win.UltraWinToolbars.ButtonTool buttonTool1 = new Infragistics.Win.UltraWinToolbars.ButtonTool("Nhập Kho");
+            Infragistics.Win.UltraWinToolbars.ButtonTool buttonTool2 = new Infragistics.Win.UltraWinToolbars.ButtonTool("NhapKho");
+            Infragistics.Win.UltraWinToolbars.ButtonTool buttonTool3 = new Infragistics.Win.UltraWinToolbars.ButtonTool("Nhập Kho");
+            Infragistics.Win.UltraWinToolbars.ButtonTool buttonTool4 = new Infragistics.Win.UltraWinToolbars.ButtonTool("NhapKho");
+            this.ultraButton1 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraToolbarsManager1 = new Infragistics.Win.UltraWinToolbars.UltraToolbarsManager(this.components);
+            this.Form1_Fill_Panel = new Infragistics.Win.Misc.UltraPanel();
+            this._Form1_Toolbars_Dock_Area_Left = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._Form1_Toolbars_Dock_Area_Right = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._Form1_Toolbars_Dock_Area_Top = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            this._Form1_Toolbars_Dock_Area_Bottom = new Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraToolbarsManager1)).BeginInit();
+            this.Form1_Fill_Panel.ClientArea.SuspendLayout();
+            this.Form1_Fill_Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ultraButton1
+            // 
+            this.ultraButton1.Location = new System.Drawing.Point(131, 125);
+            this.ultraButton1.Name = "ultraButton1";
+            this.ultraButton1.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton1.TabIndex = 0;
+            this.ultraButton1.Text = "ultraButton1";
+            // 
+            // ultraToolbarsManager1
+            // 
+            this.ultraToolbarsManager1.DesignerFlags = 1;
+            this.ultraToolbarsManager1.DockWithinContainer = this;
+            this.ultraToolbarsManager1.DockWithinContainerBaseType = typeof(System.Windows.Forms.Form);
+            ultraToolbar1.DockedColumn = 0;
+            ultraToolbar1.DockedRow = 0;
+            ultraToolbar1.NonInheritedTools.AddRange(new Infragistics.Win.UltraWinToolbars.ToolBase[] {
+            buttonTool1,
+            buttonTool2});
+            ultraToolbar1.Text = "Danh mục";
+            this.ultraToolbarsManager1.Toolbars.AddRange(new Infragistics.Win.UltraWinToolbars.UltraToolbar[] {
+            ultraToolbar1});
+            buttonTool3.SharedPropsInternal.Caption = "Nhập Kho";
+            buttonTool4.SharedPropsInternal.Caption = "NhapKho";
+            this.ultraToolbarsManager1.Tools.AddRange(new Infragistics.Win.UltraWinToolbars.ToolBase[] {
+            buttonTool3,
+            buttonTool4});
+            // 
+            // Form1_Fill_Panel
+            // 
+            // 
+            // Form1_Fill_Panel.ClientArea
+            // 
+            this.Form1_Fill_Panel.ClientArea.Controls.Add(this.ultraButton1);
+            this.Form1_Fill_Panel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Form1_Fill_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Form1_Fill_Panel.Location = new System.Drawing.Point(0, 27);
+            this.Form1_Fill_Panel.Name = "Form1_Fill_Panel";
+            this.Form1_Fill_Panel.Size = new System.Drawing.Size(800, 423);
+            this.Form1_Fill_Panel.TabIndex = 0;
+            // 
+            // _Form1_Toolbars_Dock_Area_Left
+            // 
+            this._Form1_Toolbars_Dock_Area_Left.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._Form1_Toolbars_Dock_Area_Left.BackColor = System.Drawing.SystemColors.Control;
+            this._Form1_Toolbars_Dock_Area_Left.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Left;
+            this._Form1_Toolbars_Dock_Area_Left.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._Form1_Toolbars_Dock_Area_Left.Location = new System.Drawing.Point(0, 27);
+            this._Form1_Toolbars_Dock_Area_Left.Name = "_Form1_Toolbars_Dock_Area_Left";
+            this._Form1_Toolbars_Dock_Area_Left.Size = new System.Drawing.Size(0, 423);
+            this._Form1_Toolbars_Dock_Area_Left.ToolbarsManager = this.ultraToolbarsManager1;
+            // 
+            // _Form1_Toolbars_Dock_Area_Right
+            // 
+            this._Form1_Toolbars_Dock_Area_Right.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._Form1_Toolbars_Dock_Area_Right.BackColor = System.Drawing.SystemColors.Control;
+            this._Form1_Toolbars_Dock_Area_Right.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Right;
+            this._Form1_Toolbars_Dock_Area_Right.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._Form1_Toolbars_Dock_Area_Right.Location = new System.Drawing.Point(800, 27);
+            this._Form1_Toolbars_Dock_Area_Right.Name = "_Form1_Toolbars_Dock_Area_Right";
+            this._Form1_Toolbars_Dock_Area_Right.Size = new System.Drawing.Size(0, 423);
+            this._Form1_Toolbars_Dock_Area_Right.ToolbarsManager = this.ultraToolbarsManager1;
+            // 
+            // _Form1_Toolbars_Dock_Area_Top
+            // 
+            this._Form1_Toolbars_Dock_Area_Top.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._Form1_Toolbars_Dock_Area_Top.BackColor = System.Drawing.SystemColors.Control;
+            this._Form1_Toolbars_Dock_Area_Top.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Top;
+            this._Form1_Toolbars_Dock_Area_Top.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._Form1_Toolbars_Dock_Area_Top.Location = new System.Drawing.Point(0, 0);
+            this._Form1_Toolbars_Dock_Area_Top.Name = "_Form1_Toolbars_Dock_Area_Top";
+            this._Form1_Toolbars_Dock_Area_Top.Size = new System.Drawing.Size(800, 27);
+            this._Form1_Toolbars_Dock_Area_Top.ToolbarsManager = this.ultraToolbarsManager1;
+            // 
+            // _Form1_Toolbars_Dock_Area_Bottom
+            // 
+            this._Form1_Toolbars_Dock_Area_Bottom.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this._Form1_Toolbars_Dock_Area_Bottom.BackColor = System.Drawing.SystemColors.Control;
+            this._Form1_Toolbars_Dock_Area_Bottom.DockedPosition = Infragistics.Win.UltraWinToolbars.DockedPosition.Bottom;
+            this._Form1_Toolbars_Dock_Area_Bottom.ForeColor = System.Drawing.SystemColors.ControlText;
+            this._Form1_Toolbars_Dock_Area_Bottom.Location = new System.Drawing.Point(0, 450);
+            this._Form1_Toolbars_Dock_Area_Bottom.Name = "_Form1_Toolbars_Dock_Area_Bottom";
+            this._Form1_Toolbars_Dock_Area_Bottom.Size = new System.Drawing.Size(800, 0);
+            this._Form1_Toolbars_Dock_Area_Bottom.ToolbarsManager = this.ultraToolbarsManager1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Form1_Fill_Panel);
+            this.Controls.Add(this._Form1_Toolbars_Dock_Area_Left);
+            this.Controls.Add(this._Form1_Toolbars_Dock_Area_Right);
+            this.Controls.Add(this._Form1_Toolbars_Dock_Area_Bottom);
+            this.Controls.Add(this._Form1_Toolbars_Dock_Area_Top);
+            this.Name = "Form1";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraToolbarsManager1)).EndInit();
+            this.Form1_Fill_Panel.ClientArea.ResumeLayout(false);
+            this.Form1_Fill_Panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.Misc.UltraButton ultraButton1;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsManager ultraToolbarsManager1;
+        private Infragistics.Win.Misc.UltraPanel Form1_Fill_Panel;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _Form1_Toolbars_Dock_Area_Left;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _Form1_Toolbars_Dock_Area_Right;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _Form1_Toolbars_Dock_Area_Bottom;
+        private Infragistics.Win.UltraWinToolbars.UltraToolbarsDockArea _Form1_Toolbars_Dock_Area_Top;
+    }
+}
+
---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Đây là app bán hàng
